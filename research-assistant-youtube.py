28688355441d56fff83a151cce1d8456e27ddb94
Ashvin Varghese <69413742+Ashvin-Vargez@users.noreply.github.com>
2024-03-25 21:31:53 +0530

    removed localhost and port

--- research-assistant-youtube.py
+++ research-assistant-youtube.py
@@ -226,16 +226,16 @@ app = FastAPI(
   description="A simple api server using Langchain's Runnable interfaces",
 )
 
-add_routes(
-    app,
-    chain,
-    path="/research-assistant",
-)
+# add_routes(
+#     app,
+#     chain,
+#     path="/research-assistant",
+# )
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app)
 
     
